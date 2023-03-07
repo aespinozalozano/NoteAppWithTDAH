9001e794cd58cf51d7f9a333791f2d53aa7c61f5
Ademir Espinoza <ademirjesusespinozalozano@gmail.com>
2023-03-07 15:32:37 -0500

    Archivo 8 - Login Listo Splash Listo ViewOnBoarding Listo Conectar Firebase a login Listo Fragments (Listo) Edit Note (Listo) Onboarding Modificado (listo)

--- app/src/main/java/com/example/noteappwithtdah/ui/Fragment/EditNotesFragment.kt
+++ app/src/main/java/com/example/noteappwithtdah/ui/Fragment/EditNotesFragment.kt
@@ -5,16 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.example.noteappwithtdah.R
+import com.example.noteappwithtdah.databinding.FragmentEditNotesBinding
 
 class EditNotesFragment : Fragment() {
 
+    val notes by navArgs<EditNotesFragmentArgs>()
+    lateinit var binding: FragmentEditNotesBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_notes, container, false)
+
+        binding= FragmentEditNotesBinding.inflate(layoutInflater,container,false)
+
+        binding.edTittle.setText(notes.data.title)
+        binding.edSubTittle.setText(notes.data.subTitle)
+        binding.edNotes.setText(notes.data.notes)
+
+        return binding.root
     }
 
 }
